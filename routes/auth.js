a912c00175e747929c97e2434780ff09b1e4286a
ELBAKKALI AIMANE <aimane.elb@gmail.com>
2020-05-28 16:47:39 +0000

    auth added

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,35 @@
+const validate = require('../middleware/validate')
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const { User, validateAuth } = require("../models/user");
 const express = require('express');
 const router = express.Router();
 
-router.post('/', async(req, res) => {
-    return res.send('Hello user');
+router.post('/', validate(validateAuth), async(req, res) => {
+    try {     
+        let user = await validateUser(req.body.name)
+        if(!user)   
+            return res.status(400).send('Invalid name or password.');
+
+        const validPassword = await validatePassword(req.body.password, user.password);
+        if(!validPassword)   
+            return res.status(400).send('Invalid name or password.');
+        
+        const token = user.generateAuthToken();
+        return res.send(token);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
+
+async function validateUser(name){
+    const user = await User.findOne({ name : name });
+    return user;
+}
+
+async function validatePassword(pass1, pass2){
+    const result = await bcrypt.compare(pass1, pass2);
+    return result;
+}
 
 module.exports = router;
