710d920ae50489ce4b96d3ab348be35bc32efb10
ELBAKKALI AIMANE <aimane.elb@gmail.com>
2020-05-28 16:12:06 +0000

    MongoDB connection added

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const config = require('config');
+
+
 const auth = require('./routes/auth');
 
 app.use(express.json());
 app.use('/api/auth', auth);
+
+const db = config.get('db');
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex : true,
+    retryWrites: false})
+.then(() => console.log(`Connected to ${db} ...`));
 
 const port = process.env.PORT || 3000 ;
 
