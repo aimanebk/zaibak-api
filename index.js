91b1878816f980db9d68877688deb46fe5800104
ELBAKKALI AIMANE <aimane.elb@gmail.com>
2020-05-29 00:51:59 +0000

    cors added

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const app = express();
 
 const mongoose = require('mongoose');
 const config = require('config');
+const cors = require('cors')
+
+var allowCrossDomain = function(req, res, next){
+	var allowedOrigins = ['http://localhost:4200'];
+	var origin = req.headers.origin;
+	if(allowedOrigins.indexOf(origin) > -1){
+	   res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, HEAD, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+    next();
+}
+
+app.options('*', cors())
+
+app.use(allowCrossDomain);
 
 
 const auth = require('./routes/auth');
