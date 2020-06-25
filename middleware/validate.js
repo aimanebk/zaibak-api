b55d4b95f04f80d0761fa8830731491e7317efbe
ELBAKKALI AIMANE <aimane.elb@gmail.com>
2020-06-25 22:43:08 +0100

    update validation return message

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,8 +1,10 @@
 module.exports = function(validation){
     return (req, res, next) => {
         const { error } = validation(req.body);
-        if(error)
-            return res.status(400).send({ message : error.details[0].message});
+        if(error){
+            console.log(error.details[0].message);
+            return res.status(400).send({ message : `Formulaire n'est pas valide`});
+        }
 
         next();
     }
