c6c3fd71030b021b3622fe964c9161220bcf491c
ELBAKKALI AIMANE <aimane.elb@gmail.com>
2020-07-18 16:11:31 +0100

    update cors middleware

--- startup/cors.js
+++ startup/cors.js
@@ -1,20 +1,22 @@
 const cors = require('cors')
 
-var allowCrossDomain = function(req, res, next){
-    var allowedOrigins = ['http://localhost:4200', 'https://stark-dawn-53505.herokuapp.com',
-                          'https://zaibak.web.app'];
-	var origin = req.headers.origin;
-	if(allowedOrigins.indexOf(origin) > -1){
-	   res.setHeader('Access-Control-Allow-Origin', origin);
-	}
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, HEAD, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
-    next();
-}
+// var allowCrossDomain = function(req, res, next){
+//     var allowedOrigins = ['https://localhost:4200', 'https://stark-dawn-53505.herokuapp.com',
+//                           'https://zaibak.web.app'];
+// 	var origin = req.headers.origin;
+// 	if(allowedOrigins.indexOf(origin) > -1){
+// 	   res.setHeader('Access-Control-Allow-Origin', origin);
+// 	}
+//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, HEAD, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type', '*');
+//     res.header( 'Access-Control-Allow-Credentials',true);
+//     next();
+// }
 
 
 
 module.exports = function(app){
-    app.options('*', cors())
-    app.use(allowCrossDomain);
+    app.use(cors({credentials : true , origin: ['http://localhost:4200',
+    'https://zaibak.web.app']}))
+    // app.use(allowCrossDomain);
 }
